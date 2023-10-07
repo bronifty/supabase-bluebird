809ec7c4616452d1a558d63a7045f478e3ab348d
bronifty <bronifty@gmail.com>
2023-10-07 15:01:12 -0400

    this

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AuthButtonServer from "@/app/auth-button-server";
 import { redirect } from "next/navigation";
+import { Database } from "@/lib/database.types";
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createServerComponentClient<Database>({ cookies });
   const {
     data: { session },
   } = await supabase.auth.getSession();
