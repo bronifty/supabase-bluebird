acf05d3efb17e15c6219489c72b9c67f91dd1ad0
bronifty <bronifty@gmail.com>
2023-10-07 11:04:28 -0400

    this

---
+++ app/auth-button-client.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function AuthButtonClient({ session }: { session: any }) {
+  const supabase = createClientComponentClient();
+  // const [session, setSession] = React.useState();
+  // React.useEffect(() => {
+  //   async function getSession() {
+  //     const { data } = await supabase.auth.getSession();
+  //     setSession(data.session);
+  //   }
+  //   getSession();
+  // }, []);
+  const router = useRouter();
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+  return session ? (
+    <button onClick={handleSignOut}>Log Out</button>
+  ) : (
+    <button onClick={handleSignIn}>Log In</button>
+  );
+}
---
+++ app/auth-button-server.tsx
@@ -0,0 +1,18 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButton from "./auth-button-client";
+import AuthButtonClient from "./auth-button-client";
+
+export default async function AuthButtonServer() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <>
+      <AuthButtonClient session={session} />
+    </>
+  );
+}
