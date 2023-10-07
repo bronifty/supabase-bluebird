5f9f783741b9c47c6ba41f0924db640df0781c7c
bronifty <bronifty@gmail.com>
2023-10-07 14:58:04 -0400

    this

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,13 +1,13 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse, type NextRequest } from "next/server";
 import { cookies } from "next/headers";
+import { log } from "console";
 export async function GET(request: NextRequest) {
   const requestURL = new URL(request.url);
   const code = requestURL.searchParams.get("code");
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
-    console.log("in app/auth/callback/route.ts setting the session cookie");
     await supabase.auth.exchangeCodeForSession(code);
   }
 
--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,6 @@ import { NextResponse, type NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-  console.log(
-    "in app/middleware.ts refreshing the session token and setting the cookie"
-  );
   await supabase.auth.getSession();
   return res;
 }
