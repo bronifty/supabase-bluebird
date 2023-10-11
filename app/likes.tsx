21de7d468fe24a0322a236bf2de304250bdee677
bronifty <bronifty@gmail.com>
2023-10-11 18:23:10 -0400

    main

--- app/likes.tsx
+++ app/likes.tsx
@@ -1,18 +1,23 @@
 "use client";
-
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { RouteMatcher } from "next/dist/server/future/route-matchers/route-matcher";
+import { useRouter } from "next/navigation";
 export default function Likes({ tweet }: any) {
+  const router = useRouter();
   console.log(tweet);
-  async function handleClick(event) {
+  async function handleClick() {
     const supabase = createClientComponentClient<Database>();
+
     const {
       data: { user },
     } = await supabase.auth.getUser();
-    await supabase
-      .from("likes")
-      .insert({ user_id: user.id, tweet_id: tweet.id });
+    if (user) {
+      await supabase
+        .from("likes")
+        .insert({ user_id: user.id, tweet_id: tweet.id });
+      router.refresh();
+    }
   }
-
   return (
     <>
       <button onClick={handleClick}>{tweet.likes.length} Likes</button>
